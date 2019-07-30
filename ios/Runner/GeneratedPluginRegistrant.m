a7e287ce1b8ccae5283fbc4915b58a5e4acb3ff3
Melvin Salas <melvinsalasrivera@gmail.com>
2019-07-29 23:28:12 -0600

    List all devices Apps

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -3,10 +3,12 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
+#import <device_apps/DeviceAppsPlugin.h>
 
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [DeviceAppsPlugin registerWithRegistrar:[registry registrarForPlugin:@"DeviceAppsPlugin"]];
 }
 
 @end
